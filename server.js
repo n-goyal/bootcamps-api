91473b16980b6967f8543e9dcfcdc6312be3e478
Nitin Goyal <goyalnitin634@gmail.com>
2021-04-11 18:08:47 +0530

    add database configs

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 
+const connectDB = require("./config/db");
 // get environments configurations
 dotenv.config({ path: "./config/config.env" });
 const port = process.env.PORT || 5000;
 
+// connect db
+connectDB();
 // import routers
 const bootcamps = require("./routes/bootcamps.router");
+const { connect } = require("./routes/bootcamps.router");
 
 const app = express();
 
@@ -19,6 +24,17 @@ if (process.env.NODE_ENV === "development") {
 // routes
 app.use("/api/v1/bootcamps", bootcamps);
 
-app.listen(port, () => {
-  console.log(`server is running in ${process.env.NODE_ENV} mode at ${port}`);
+const server = app.listen(port, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode at ${port}`.yellow.bold
+  );
+});
+
+// handle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server, exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
