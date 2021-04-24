9a7dc355364552e3f21d6f2525098562f20dc616
Nitin Goyal <goyalnitin634@gmail.com>
2021-04-24 09:48:00 +0530

    add forgot and reset password functionality

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, getMe } = require("../controllers/auth.controller");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatepassword,
+} = require("../controllers/auth.controller");
 
 const { protect } = require("../middlewares/auth.middleware");
 
 router.post("/register", register);
 router.post("/login", login);
+
 router.get("/me", protect, getMe);
+router.put("/updateDetails", protect, updateDetails);
+router.put("/updatePassword", protect, updatepassword);
+
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
